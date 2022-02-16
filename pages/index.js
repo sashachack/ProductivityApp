6e38789adda1aed8cd7decd66b009658a6d1790f
SashaChackalackalBTB <schackalackal@beatthebookinc.com>
2022-02-15 19:21:20 -0600

    components folder

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { Button, Checkbox } from "@mantine/core";
+import Layout from "../components/layout.js";
 
 export default function Home() {
     return (
@@ -14,9 +15,7 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div>Hello</div>
-            <Button>Settings</Button>
-            <Checkbox></Checkbox>
+            <Layout />
         </div>
     );
 }
