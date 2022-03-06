d19327212efba787a87a857fda7ce2add095dcc2
nashsolon <i.solon@wustl.edu>
2022-03-06 17:19:50 -0600

    user post/get

--- pages/api/post_user.js
+++ pages/api/post_user.js
@@ -1,11 +1,11 @@
 import clientPromise from "./mongodb";
 
-//API endpoint for adding tasks
+//API endpoint for adding user
 
 export default async (req, res) => {
     const client = await clientPromise;
     const db = client.db("Task_Manager");
       let bodyObject = JSON.parse(req.body);
-      let newTask = await db.collection("Tasks").insertOne(bodyObject);
+      let newTask = await db.collection("users").insertOne(bodyObject);
       res.json(newTask.ops[0]);
 }
