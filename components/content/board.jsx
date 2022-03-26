e2bb531b5496ca720b33fd949a381b017f1802b9
Skylar <skylarfong@wustl.edu>
2022-03-26 16:46:47 -0500

    drag and drop

--- components/content/board.jsx
+++ components/content/board.jsx
@@ -1,5 +1,7 @@
 import BoardCard from "../elements/board_card";
 import { Space, SimpleGrid, Title, Card } from "@mantine/core";
+import { DragDropContext, Droppable, Draggable , DragItem} from 'react-beautiful-dnd';
+import { useState, useEffect } from 'react';
 
 let Board = ({ content, setContent, clickCard, addCard }) => {
     // console.log(content);
@@ -8,7 +10,10 @@ let Board = ({ content, setContent, clickCard, addCard }) => {
     for (let status of statuses) {
         items[status] = content.filter((c) => c.status == status);
     }
-
+    const [tasks, updateTasks] = useState(items);
+    useEffect(() => {
+        updateTasks(items);
+      }, []);
     // let clickCard = (id) => {
     //     console.log(id);
     // };
@@ -30,6 +35,34 @@ let Board = ({ content, setContent, clickCard, addCard }) => {
             </Card>
         );
     };
+    const removeFromList = (items, index) => {
+        const result = Array.from(items);
+        const [removed] = result.splice(index, 1);
+        return [removed, result];
+    };
+      
+    const addToList = (items, index, element) => {
+        const result = Array.from(items);
+        result.splice(index, 0, element);
+        return result;
+    };
+
+    const onDragEnd = (result) => {  
+        if (!result.destination) {  
+            return;  
+        }  
+        const copy = { ...tasks }  
+    
+        const sourceList = copy[result.source.droppableId]  
+        const [removedElement, newSourceList] = removeFromList(sourceList, result.source.index)  
+        copy[result.source.droppableId] = newSourceList  
+    
+        const destinationList = copy[result.destination.droppableId]  
+        copy[result.destination.droppableId] = addToList(destinationList, result.destination.index, removedElement)  
+    
+        updateTasks(copy)  
+    }
+    
 
     return (
         <div>
@@ -43,25 +76,42 @@ let Board = ({ content, setContent, clickCard, addCard }) => {
                 ))}
             </SimpleGrid>
             <Space h="md"></Space>
+            <DragDropContext onDragEnd={onDragEnd}>
             <SimpleGrid cols={statuses.length}>
                 {statuses.map((s, i) => (
-                    <div key={i}>
-                        {items[s].map((c, j) => (
-                            <div key={j}>
-                                <BoardCard
-                                    taskName={c.title}
-                                    label={c.label}
-                                    dueDate={c.dueDate}
-                                    id={c["_id"]}
-                                    click={(id) => clickCard(id)}
-                                />
-                                <Space h="md" />
-                            </div>
-                        ))}
-                        {addCardButton(s)}
-                    </div>
-                ))}
+                        <Droppable droppableID ={`${s}`}>
+                            {(provided) => ( 
+                                <div key={i} {...provided.droppableProps} ref={provided.innerRef}>
+                                    {items[s].map((c, j) => (
+                                        <Draggable draggableId={c.id} index={j}>
+                                            {(provided, snapshot) => (
+                                                <div key={j} ref={provided.innerRef} 
+                                                    snapshot={snapshot} 
+                                                    {...provided.draggableProps} 
+                                                    {...provided.dragHandleProps}
+                                                >
+                                                {/* <div key={j} ref={provided.innerRef} {...provided.droppableProps} ref={provided.dragHandleProps}> */}
+                                                    <BoardCard
+                                                        taskName={c.title}
+                                                        label={c.label}
+                                                        dueDate={c.dueDate}
+                                                        id={c["_id"]}
+                                                        click={(id) => clickCard(id)}
+                                                    />
+                                                    <Space h="md" />
+                                                {/* </div> */}
+                                                </div>
+                                            )}
+                                        </Draggable>
+                                    ))} 
+                                {addCardButton(s)}
+                                {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>  
+                    ))}
             </SimpleGrid>
+            </DragDropContext>
         </div>
     );
 };
