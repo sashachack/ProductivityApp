fd61aa1b6c10441bb9348f80a77c37ae4e4818a6
Skylar <skylarfong@wustl.edu>
2022-03-27 22:36:38 -0500

    dnd frontend

--- components/content/board.jsx
+++ components/content/board.jsx
@@ -11,8 +11,11 @@ let Board = ({ content, setContent, clickCard, addCard }) => {
         items[status] = content.filter((c) => c.status == status);
     }
     const [tasks, updateTasks] = useState(items);
+    console.log('ITEMS INITIAL',items)
+    console.log('TASKS INITIAL', tasks)
     useEffect(() => {
-        updateTasks(items);
+        updateTasks(tasks);
+        console.log("task !!!!!!!")
       }, []);
     // let clickCard = (id) => {
     //     console.log(id);
@@ -38,12 +41,16 @@ let Board = ({ content, setContent, clickCard, addCard }) => {
     const removeFromList = (items, index) => {
         const result = Array.from(items);
         const [removed] = result.splice(index, 1);
+        console.log('removed task', removed)
+
         return [removed, result];
     };
       
     const addToList = (items, index, element) => {
         const result = Array.from(items);
+        console.log('element', element)
         result.splice(index, 0, element);
+        console.log('task added list', result)
         return result;
     };
 
@@ -51,16 +58,25 @@ let Board = ({ content, setContent, clickCard, addCard }) => {
         if (!result.destination) {  
             return;  
         }  
+        console.log("DRAGGED")
+        console.log('tasks list', items)
+
         const copy = { ...tasks }  
-    
-        const sourceList = copy[result.source.droppableId]  
-        const [removedElement, newSourceList] = removeFromList(sourceList, result.source.index)  
-        copy[result.source.droppableId] = newSourceList  
-    
-        const destinationList = copy[result.destination.droppableId]  
-        copy[result.destination.droppableId] = addToList(destinationList, result.destination.index, removedElement)  
-    
-        updateTasks(copy)  
+        console.log('copy list',copy)
+
+        const startStatusList = copy[result.source.droppableId]  
+        console.log('start status list', startStatusList)
+
+        const [removedElement, newStartStatusList] = removeFromList(startStatusList, result.source.index)  
+        copy[result.source.droppableId] = newStartStatusList  
+        console.log('new start status' , newStartStatusList)
+
+        const endStatusList = copy[result.destination.droppableId]  
+        copy[result.destination.droppableId] = addToList(endStatusList, result.destination.index, removedElement)  
+        
+        console.log('end status list', copy[result.destination.droppableId])
+        console.log('FINAL LISTS', copy)
+        updateTasks(copy);  
     }
     
 
@@ -79,29 +95,23 @@ let Board = ({ content, setContent, clickCard, addCard }) => {
             <DragDropContext onDragEnd={onDragEnd}>
             <SimpleGrid cols={statuses.length}>
                 {statuses.map((s, i) => (
-                        <Droppable droppableID ={`${s}`}>
-                            {(provided) => ( 
+                        <Droppable key={i} droppableId ={s}>
+                            {(provided, snapshot) => ( 
                                 <div key={i} {...provided.droppableProps} ref={provided.innerRef}>
                                     {items[s].map((c, j) => (
-                                        <Draggable draggableId={c.id} index={j}>
-                                            {(provided, snapshot) => (
-                                                <div key={j} ref={provided.innerRef} 
-                                                    snapshot={snapshot} 
-                                                    {...provided.draggableProps} 
-                                                    {...provided.dragHandleProps}
-                                                >
-                                                {/* <div key={j} ref={provided.innerRef} {...provided.droppableProps} ref={provided.dragHandleProps}> */}
-                                                    <BoardCard
-                                                        taskName={c.title}
-                                                        label={c.label}
-                                                        dueDate={c.dueDate}
-                                                        id={c["_id"]}
-                                                        click={(id) => clickCard(id)}
-                                                    />
-                                                    <Space h="md" />
-                                                {/* </div> */}
-                                                </div>
-                                            )}
+                                        <Draggable key={c["_id"]} draggableId={c["_id"]} index={j}>
+                                        {(provided) => (
+                                            <div ref={provided.innerRef} snapshot={snapshot} {...provided.dragHandleProps} {...provided.draggableProps} >
+                                                <BoardCard
+                                                    taskName={c.title}
+                                                    label={c.label}
+                                                    dueDate={c.dueDate}
+                                                    id={c["_id"]}
+                                                    click={(id) => clickCard(id)}
+                                                />
+                                                <Space h="md" />
+                                            </div>
+                                        )}
                                         </Draggable>
                                     ))} 
                                 {addCardButton(s)}
