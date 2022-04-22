3a2aa7472fb3d6eb5f93c4ab3022aa4a262c65f4
nashsolon <i.solon@wustl.edu>
2022-04-22 12:40:02 -0500

    labels now drop down

--- pages/api/get_labels.js
+++ pages/api/get_labels.js
@@ -3,14 +3,15 @@ import clientPromise from "./mongodb";
 //API endpoint for grabbing tasks
 
 export default async(req, res) => {
-    // console.log('Get tasks')
+    
     const client = await clientPromise;
     const db = client.db("Task_Manager");
     let bodyObject = JSON.parse(req.body);
-    // console.log(bodyObject)
-    // console.log(bodyObject.collectionID)
-    const tasks = await db.collection("Tasks").find({ email: bodyObject.email,  
-                                                        collection_id: bodyObject.collectionID}).toArray();
+    console.log(bodyObject)
+    console.log('Get labels')
+ 
+    const tasks = await db.collection("labels").find({ email: bodyObject.email,  
+                                                        }).toArray();
     res.json({ status: 200, data: tasks });
 
 }
